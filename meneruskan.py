1474927af98136e15483b6665f347e9d058059a7
NaNan <61611615+Nakakari@users.noreply.github.com>
2020-07-13 14:06:28 +0700

    Create meneruskan.py
    melanjutkan percobaan kemarin. mohon maaf jika tidak sistematis.

---
+++ meneruskan.py
@@ -0,0 +1,27 @@
+print("Hello world!")
+print("\n")
+a = 12
+b = 10
+
+# percabangan if
+if a>b :
+	print("A lebih dari b")
+
+# mengetahui angka genap atau ganjil
+print("\n")
+a = 7
+print(a)
+if (a%2 == 0):
+  print("angka genap")
+if (a%2 != 0):
+  print("angka ganjil")
+
+print("\n")
+# percabangan if else 
+a = 7
+print(a)
+if (a%2 == 0):
+  print("angka genap")
+else:
+  print("angka ganjil")
+
